978653202f0ff29712aa48a1f66b47db40730249
Your Name <lqh2893535402@163.com>
2018-03-20 19:43:06 +0800

    3次提交

--- boke/database/config.js
+++ boke/database/config.js
@@ -1,1 +1,1 @@
-var mongoose = require('mongoose');mongoose.connect('mongodb://localhost/DNF');db = mongoose.connection;db.on('error', console.error.bind(console, 'connection error:'));db.once('open', function() {    console.log('数据库连接成功')});
+var mongoose = require('mongoose');mongoose.connect('mongodb://localhost/panpan');db = mongoose.connection;db.on('error', console.error.bind(console, 'connection error:'));db.once('open', function() {    console.log('数据库连接成功')});
